86df4d5632dac3790f141524284e3189d953d9aa
Dave Nash <119833142+davidjnash2@users.noreply.github.com>
2022-12-15 21:16:27 -0600

    Update java.js

--- java.js
+++ java.js
@@ -1,1 +1,11 @@
+<script>
+function myFunction() {
+  document.getElementById("profile").src="F07335DE-9DA6-41A4-BE53-D9377952592F.jpg";
+}
+</script>
 
+<script>
+function myFunction2() {
+  document.getElementById("profile").src="IMG_6499.jpeg";
+}
+</script>
