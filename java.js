07cc084be3dabd614826945990e09b2f7af6d2d6
Dave Nash <119833142+davidjnash2@users.noreply.github.com>
2022-12-18 23:03:09 -0600

    Update java.js

--- java.js
+++ java.js
@@ -3,5 +3,5 @@ let displayImage = document.getElementById ("profile")
 if (displayImage.src.match("F07335DE-9DA6-41A4-BE53-D9377952592F.jpg")){
 displayImage.src = "IMG_6499.jpeg"
 } else {
-displayImage src = "F07335DE-9DA6-41A4-BE53-D9377952592F.jpg"}
+displayImage.src = "F07335DE-9DA6-41A4-BE53-D9377952592F.jpg"}
 }
