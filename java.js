843d0c3813480c98cc7b29e31ce1c046c8f1a212
Dave Nash <119833142+davidjnash2@users.noreply.github.com>
2022-12-18 22:31:36 -0600

    Update java.js

--- java.js
+++ java.js
@@ -1,14 +1,5 @@
-let profile = document.getElementById("profile");
-let allbusiness = document.getElementById("allbusiness");
+button.getElementById("allbusiness").addEventListener("click", myFunction);
 
-function businessDad() {
-
-    if (profile.getAttribute('src') === "https://github.com/davidjnash2/davidjnash2.github.io/blob/4471d52ab185c266fd9ec5f40b1beff3677b7ea8/F07335DE-9DA6-41A4-BE53-D9377952592F.jpg") {
-        profile.setAttribute('src', "https://github.com/davidjnash2/davidjnash2.github.io/blob/4471d52ab185c266fd9ec5f40b1beff3677b7ea8/IMG_6499.jpeg");
-    }
-    else {
-        profile.setAttribute('src', "https://github.com/davidjnash2/davidjnash2.github.io/blob/4471d52ab185c266fd9ec5f40b1beff3677b7ea8/F07335DE-9DA6-41A4-BE53-D9377952592F.jpg");
-    }
+function myFunction() {
+  img.getElementById("profile").src = "https://github.com/davidjnash2/davidjnash2.github.io/blob/4471d52ab185c266fd9ec5f40b1beff3677b7ea8/IMG_6499.jpeg";
 }
-
-allbusiness.addEventListener("click", businessDad);
