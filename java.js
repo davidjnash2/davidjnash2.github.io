f316c1d705748f60f2d5cb27f6a5326796109650
Dave Nash <119833142+davidjnash2@users.noreply.github.com>
2022-12-18 22:12:55 -0600

    Update java.js

--- java.js
+++ java.js
@@ -3,11 +3,11 @@ let allbusiness = document.getElementById("allbusiness");
 
 function businessDad() {
 
-    if (profile.getAttribute('src') === "F07335DE-9DA6-41A4-BE53-D9377952592F.jpg") {
-        profile.setAttribute('src', "IMG_6499.jpeg");
+    if (profile.getAttribute('src') === "https://github.com/davidjnash2/davidjnash2.github.io/blob/4471d52ab185c266fd9ec5f40b1beff3677b7ea8/F07335DE-9DA6-41A4-BE53-D9377952592F.jpg") {
+        profile.setAttribute('src', "https://github.com/davidjnash2/davidjnash2.github.io/blob/4471d52ab185c266fd9ec5f40b1beff3677b7ea8/IMG_6499.jpeg");
     }
     else {
-        profile.setAttribute('src', "F07335DE-9DA6-41A4-BE53-D9377952592F.jpg");
+        profile.setAttribute('src', "https://github.com/davidjnash2/davidjnash2.github.io/blob/4471d52ab185c266fd9ec5f40b1beff3677b7ea8/F07335DE-9DA6-41A4-BE53-D9377952592F.jpg");
     }
 }
 
