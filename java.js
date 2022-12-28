68241f8ca404d7b57157d21fce32f1b8747de96e
Dave Nash <119833142+davidjnash2@users.noreply.github.com>
2022-12-27 23:49:05 -0600

    Update java.js

--- java.js
+++ java.js
@@ -1,6 +1,14 @@
-function changeImage()
-{
-var img = document.getElementById("profile");
-img.src="IMG_6499.jpeg";
-return false;
+let profile = document.getElementById("profile");
+let allbusiness = document.getElementById("allbusiness");
+
+function businessDad() {
+
+    if (profile.getAttribute('src') === "F07335DE-9DA6-41A4-BE53-D9377952592F.jpg") {
+        profile.setAttribute('src', "IMG_6499.jpeg");
+    }
+    else {
+        profile.setAttribute('src', "F07335DE-9DA6-41A4-BE53-D9377952592F.jpg");
+    }
 }
+
+allbusiness.addEventListener("click", businessDad);
